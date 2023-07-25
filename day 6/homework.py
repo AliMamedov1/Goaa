7d1b30fa02b43f0511a9ce408bf07edf079b8875
AliMamedov1 <rarah2274@gmail.com>
2023-07-25 19:35:04 +0400

    third commit

---
+++ day 6/homework.py
@@ -0,0 +1,15 @@
+# მომხმარებელს შემოატანინეთ მისი ასაკი და დაპრინტეთ არის თუ არა 18 წელზე ნაკლების (True და False იგულისხმება)
+# მომხმარებელს შეეკითხეთ მამამისის და მისი ასაკები და დაპრინტეთ არის თუ არა 60 წელზე მეტი
+# და Sololearn სადამდეც გავიარეთ იქამდე გააკეთეთ ყველაფერი
+
+
+
+your_age = input("Whats your age: ")
+print(int(your_age) > 18)
+dad_age = input("whats your dads age: ")
+our_age = dad_age + your_age
+print(int(our_age) > 50)
+
+
+
+
