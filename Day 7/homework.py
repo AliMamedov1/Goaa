96fadd9dc8d36915e2f861b18e2899827c882948
AliMamedov1 <rarah2274@gmail.com>
2023-07-27 21:59:54 +0400

    fourth commit

---
+++ Day 7/homework.py
@@ -0,0 +1,27 @@
+name= "Ali"
+
+name = True
+
+surname = "Mamedov"
+
+age = 13
+
+age2 = 25
+
+age= False
+
+age2= True
+
+surname= True
+
+goa_best=True
+
+another_academy=False
+
+print(name and surname)
+
+print(goa_best or another_academy)
+
+print(age and age2)
+
+
