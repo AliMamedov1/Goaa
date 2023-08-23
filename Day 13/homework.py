391beccfea3de6129d702b559a996da2d347ee7e
AliMamedov1 <rarah2274@gmail.com>
2023-08-23 21:19:38 +0400

    idk x2  commit

--- Day 13/homework.py
+++ Day 13/homework.py
@@ -5,7 +5,7 @@ tanxmovnebi = 0
 
 while i < len(my_name):
     char = my_name[i]   
-    if char != "a" and char != "i" and char != "e" and char != "o" and char != "i" :
+    if char != "a" and char != "i" and char != "e" and char != "o" and char != "i" and char != "u":
         tanxmovnebi = tanxmovnebi + 1
     i = i+1  
 
